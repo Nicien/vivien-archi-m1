9f4c642e6c9735ec571e80aeb94a5555e8784d49
Raphael <raphaelarzoin173@gmail.com>
2025-10-21 15:24:45 +0200

    feat[backend] init

--- raphael/raphael.py
+++ raphael/raphael.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/raphael")
+async def raph():
+    return {"error": 404, "message": "not found"}
