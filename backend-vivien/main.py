c16499a50ee57445e88e2df160ce9c333b01d86a
Vivien-alfred-eyes <vivien.miniussi@alfred-eyes.ai>
2025-10-22 06:08:27 +0200

    feat(backend): GET /grid

--- backend-vivien/main.py
+++ backend-vivien/main.py
@@ -1,15 +1,27 @@
-from typing import Union
-
+from typing import List
+from pydantic.dataclasses import dataclass
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+@dataclass
+class CellContent:
+    playerName: str | None = None
+
+@dataclass
+class Grid:
+    width: int
+    height: int
+    cells: List[CellContent]
+
+
+@app.get("/grid")
+async def read_grid() -> Grid:
+    playersA = CellContent(playerName="Player A")
+    return Grid(width=3, height=3, cells=[playersA, *[CellContent() for _ in range(8)]])
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"Hello": "Toto"}
