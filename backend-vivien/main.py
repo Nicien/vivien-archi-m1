079f308555ca5a41b7bef14ed3534770f9df24bc
Vivien Miniussi <vivien.miniussi@gmail.com>
2025-11-18 14:02:12 +0100

    typescript checks (#10)
    Co-authored-by: Vivien-alfred-eyes <vivien.miniussi@alfred-eyes.ai>

--- backend-vivien/main.py
+++ backend-vivien/main.py
@@ -1,6 +1,6 @@
 from typing import List
 from pydantic.dataclasses import dataclass
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,7 +13,8 @@ app.add_middleware(
 
 @dataclass
 class CellContent:
-    playerName: str | None = None
+    selected: bool
+    caption: str | None = None
 
 @dataclass
 class Grid:
@@ -21,18 +22,24 @@ class Grid:
     height: int
     cells: List[CellContent]
 
+GRID_SIZE = 10
 grid = Grid(
-    width=3,
-    height=3,
-    cells=[CellContent() for _ in range(9)]
+    width=GRID_SIZE,
+    height=GRID_SIZE,
+    cells=[CellContent(selected=False) for _ in range(GRID_SIZE**2)]
 )
 
 @app.get("/grid")
 async def read_grid() -> Grid:
-    return grid  
+    return grid
+
+@dataclass
+class UpdateBody:
+    caption: str
 
 @app.post("/cell/{cell_index}")
-async def update_cell(cell_index: int, body: dict = Body(...)):
+async def update_cell(cell_index: int, body: UpdateBody):
     if 0 <= cell_index < len(grid.cells):
-        grid.cells[cell_index].playerName = body.get("playerName")
+        grid.cells[cell_index].caption = body.caption
+        grid.cells[cell_index].selected = True
     return grid
