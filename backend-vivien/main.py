cad7e2e40c77ec0ae30132cc95ac4f3fe3b89ace
Kouceyla <kouceyla.aliouche@ortmontreuil.fr>
2025-12-16 13:59:12 +0100

    feature: add websockets

--- backend-vivien/main.py
+++ backend-vivien/main.py
@@ -1,9 +1,11 @@
 from typing import List
+from dataclasses import asdict # Pour convertir la grille en JSON
 from pydantic.dataclasses import dataclass
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -29,6 +31,26 @@ grid = Grid(
     cells=[CellContent(selected=False) for _ in range(GRID_SIZE**2)]
 )
 
+active_clients: List[WebSocket] = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    active_clients.append(websocket) 
+    try:
+        await websocket.send_json(asdict(grid))
+        
+        while True:
+            await websocket.receive_text()
+            
+    except WebSocketDisconnect:
+        active_clients.remove(websocket)
+
+async def broadcast_grid():
+    grid_dict = asdict(grid)
+    for client in active_clients:
+        await client.send_json(grid_dict)
+
 @app.get("/grid")
 async def read_grid() -> Grid:
     return grid
@@ -42,4 +64,7 @@ async def update_cell(cell_index: int, body: UpdateBody):
     if 0 <= cell_index < len(grid.cells):
         grid.cells[cell_index].caption = body.caption
         grid.cells[cell_index].selected = True
+        
+        await broadcast_grid()
+        
     return grid
